fed2b08648c8f7decd242ccbeeed64bc7b002742
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 01:31:35 +0600

    feat: reac stream done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,44 @@
+import fs from "fs";
+import process from "process";
+import stream from "stream";
+
+const path = new URL("./files/fileToRead.txt", import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const fileContent = fs.readFileSync(path, "utf-8");
+
+    const readableStream = stream.Readable.from(fileContent);
+
+    readableStream.pipe(process.stdout);
+
+    readableStream.on("end", () => {
+      console.log("\nFile reading completed.");
+    });
+
+  } catch (error) {
+    console.log("STREAM operation failed");
+  }
 };
 
 await read();
+
+
+/* 
+const read = async () => {
+  try {
+    const readable = fs.createReadStream(path, "utf-8");
+
+    readable.on("error", (error) => console.log(`error: ${error.message}`));
+
+    readable.on("data", (chunk) => process.stdout.write(chunk));
+
+    readable.on("end", () => console.log("\nFile reading completed."));
+
+  } catch (error) {
+    console.log("STREAM operation failed");
+  }
+};
+
+await read(); 
+*/
