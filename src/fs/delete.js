5d002ce0a3cfc4c7cf304092882383a3eb295206
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 00:30:30 +0600

    feat: delete done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from 'fs'
+
+const path = new URL('./files/fileToRemove.txt', import.meta.url);
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.unlink(path)
+        console.log(`File fileToRemove.txt created successfully`);
+    } catch (error) {
+        console.log('FS operation failed')
+    }
 };
 
 await remove();
