7589fd0d4fcaaadb2122eb96dd3876244298d751
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 00:20:21 +0600

    feat: copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import fs from "fs";
+
+const filesCopy = new URL("../fs/files_copy", import.meta.url);
+const filesCurrent = new URL(decodeURI("./files"), import.meta.url);
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.promises.mkdir(filesCopy, { recursive: true });
+    const files = await fs.promises.readdir(filesCurrent);
+
+    await Promise.all(
+      files.map(async (el) => {
+        try {
+          await fs.promises.access(`${filesCurrent.pathname}/${el}`, fs.constants.R_OK);
+          await fs.promises.copyFile(
+            `${filesCurrent.pathname}/${el}`,
+            `${filesCopy.pathname}/${el}`,
+            fs.constants.COPYFILE_EXCL
+          );
+          console.log(`File ${el} copied successfully.`);
+        } catch (error) {
+          console.error(`Error copying file ${el}:`, error);
+        }
+      })
+    );
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await copy();
