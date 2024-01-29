99cd3d61440855feaa4b9750f5bf26bf12248625
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 00:28:38 +0600

    feat: create done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
+const path = new URL("./files/fresh.txt", import.meta.url);
+const content = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.promises.writeFile(path, content);
+    console.log(`File fresh.txt with content ${content} created successfully`);
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await create();
