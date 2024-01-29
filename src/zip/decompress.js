ad1f77c226e78d5a332ca9524fcf8e4e709ebcb4
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 03:11:22 +0600

    feat: decompress done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+
+const archiveFile = new URL("archive.gz", import.meta.url);
+const decompressFile = new URL("fileToCompress.txt", import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const readable = createReadStream(archiveFile);
+    const writable = createWriteStream(decompressFile);
+    const gunzipStream = createGunzip();
+
+    await pipeline(readable, gunzipStream, writable);
+
+    console.log("Decompression successful!");
+  } catch (error) {
+    console.error("Error Decompression failed:", error);
+  }
 };
 
 await decompress();
