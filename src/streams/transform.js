8606c77ba930a06250e4b9cde1e36e8852753d0f
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 01:49:32 +0600

    feat: transform stream done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import process from "process";
+import stream from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const readable = process.stdin;
+
+    const reverse = new stream.Transform({
+      transform(chunk, encoding, callback) {
+        const reversedStdin = chunk.toString().split("").reverse().join("");
+
+        this.push(reversedStdin);
+
+        callback();
+      },
+    });
+
+    readable.pipe(reverse).pipe(process.stdout);
+  } catch (error) {}
 };
 
 await transform();
