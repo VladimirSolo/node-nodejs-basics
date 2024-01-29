4417020abd49ced0a53ed651240b9de6c2bafb2d
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 00:40:17 +0600

    feat: rename done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fs from 'fs'
+
+const oldPath = new URL('./files/wrongFilename.txt', import.meta.url);
+const newPath = new URL('./files/properFilename.md', import.meta.url);
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.rename(oldPath, newPath)
+        console.log('File rename successfully')
+       } catch (error) {
+        console.log('FS operation failed')
+       }
 };
 
 await rename();
