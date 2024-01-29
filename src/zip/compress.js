dfc0ab86da441533468ef74babcf26ca883dfa40
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 02:16:08 +0600

    feat: compress done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+const pipelineAsync = promisify(pipeline);
+const compressFile = new URL("./files/fileToCompress.txt", import.meta.url);
+const archiveFileName = "archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(compressFile);
+  const gzipStream = createGzip();
+
+  const writeStream = createWriteStream(archiveFileName);
+
+  try {
+    await pipelineAsync(readStream, gzipStream, writeStream);
+    console.log("Compression successfully complete.");
+
+  } catch (error) {
+    console.error("Error compression:", error);
+  }
 };
 
-await compress();
+compress();
