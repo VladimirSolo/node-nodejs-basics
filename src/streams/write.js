36421a3e87ed9ee55214765a36c6a6e319dcaca3
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 01:37:42 +0600

    feat: write stream done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import process from "process";
+import stream from "stream";
+
+const path = new URL("./files/fileToWrite.txt", import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const writable = fs.createWriteStream(path);
+
+    await stream.promises.pipeline(process.stdin, writable);
+  } catch (error) {
+    console.log("STREAM operation failed");
+  }
 };
 
 await write();
