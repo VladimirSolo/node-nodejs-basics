af4957ae0bd6ada99cb33dc048bed1ebd22ecf34
Vladimir Solo <freeman.wushu@mail.ru>
2024-01-30 00:37:04 +0600

    feat: read done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from 'fs'
+
+const path = new URL('./files/fileToRead.txt', import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const readFile = await fs.promises.readFile(path, 'utf-8');
+        console.log(readFile);
+       } catch (error) {
+        console.log('FS operation failed');
+       }
 };
 
 await read();
