57b80b2be1370b58cc25c2627c3ef3b786b3d079
Vladimir Solo <freeman.wushu@mail.ru>
2025-10-26 21:45:41 +0300

    feat: add solution cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const childProcess = spawn("node", ["files/script.js", ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+  });
+
+  childProcess.on("error", (error) => {
+    console.error(`Error spawning child process: ${error.message}`);
+    process.exit(1);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["testArg_1", "testArg_2", "testArg_3", "testArg_4"]);
