9c8db97bc92f554751e4b3212a3b57a192412bfa
Vladimir Solo <freeman.wushu@mail.ru>
2025-10-26 20:19:20 +0300

    feat: add solution hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import crypto from "crypto";
+import fs from "fs";
+
+const path = new URL("./files/fileToCalculateHashFor.txt", import.meta.url);
+
 const calculateHash = async () => {
-  // Write your code here
+  try {
+    const fileBuffer = await fs.promises.readFile(path);
+    const hash = crypto.createHash("SHA3-256").update(fileBuffer).digest("hex");
+    console.log({ hash });
+  } catch (error) {
+    console.log("Falled Hash");
+  }
 };
 
 await calculateHash();
