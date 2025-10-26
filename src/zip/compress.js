414848c0ee6ebddedb6e1421b4a87ab055d1041d
Vladimir Solo <freeman.wushu@mail.ru>
2025-10-26 20:36:16 +0300

    feat: add solution zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import zlib from "zlib";
+import { URL } from "url";
+
+const compressFile = new URL("./files/fileToCompress.txt", import.meta.url);
+const archiveFileName = new URL("./archive.gz", import.meta.url);
+
 const compress = async () => {
-  // Write your code here
+  try {
+    const readable = createReadStream(compressFile);
+    const writable = createWriteStream(archiveFileName);
+    const gzipStream = zlib.createGzip();
+
+    await pipeline(readable, gzipStream, writable);
+
+    console.log("Compression successful!");
+  } catch (error) {
+    console.error("Compression failed:", error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { access } from "fs/promises";
+
+const archiveFile = new URL("archive.gz", import.meta.url);
+const decompressFile = new URL("fileToCompress.txt", import.meta.url);
+
 const decompress = async () => {
-  // Write your code here
+  try {
+    await access(archiveFile);
+
+    const readable = createReadStream(archiveFile);
+    const writable = createWriteStream(decompressFile);
+    const gunzipStream = createGunzip();
+
+    await pipeline(readable, gunzipStream, writable);
+
+    console.log("Decompression successful!");
+  } catch (error) {
+    console.error("Error Decompression failed:", error);
+  }
 };
 
 await decompress();
